d10fbb6f2cc57d6327e33c0fb7fa265d6303700a
Rajat Mishra <77861069+mst-rajatmishra@users.noreply.github.com>
2024-09-13 17:04:17 +0530

    Update face_analyser.py

--- modules/face_analyser.py
+++ modules/face_analyser.py
@@ -1,13 +1,15 @@
-from typing import Any
+from typing import Any, Optional, List
 import insightface
 
 import modules.globals
 from modules.typing import Frame
 
-FACE_ANALYSER = None
+FACE_ANALYSER: Optional[insightface.app.FaceAnalysis] = None
 
-
-def get_face_analyser() -> Any:
+def get_face_analyser() -> insightface.app.FaceAnalysis:
+    """
+    Initializes and returns the face analyzer.
+    """
     global FACE_ANALYSER
 
     if FACE_ANALYSER is None:
@@ -15,16 +17,20 @@ def get_face_analyser() -> Any:
         FACE_ANALYSER.prepare(ctx_id=0, det_size=(640, 640))
     return FACE_ANALYSER
 
-
-def get_one_face(frame: Frame) -> Any:
-    face = get_face_analyser().get(frame)
+def get_one_face(frame: Frame) -> Optional[insightface.app.Face]:
+    """
+    Returns the face with the smallest x-coordinate in the bounding box from the given frame.
+    """
+    faces = get_face_analyser().get(frame)
     try:
-        return min(face, key=lambda x: x.bbox[0])
+        return min(faces, key=lambda x: x.bbox[0])
     except ValueError:
         return None
 
-
-def get_many_faces(frame: Frame) -> Any:
+def get_many_faces(frame: Frame) -> Optional[List[insightface.app.Face]]:
+    """
+    Returns all faces detected in the given frame.
+    """
     try:
         return get_face_analyser().get(frame)
     except IndexError:
